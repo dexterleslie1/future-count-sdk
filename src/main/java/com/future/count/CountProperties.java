87230dcf00323e90fac256c5b80f570acf07c810
Dexter.Chen <dexterleslie@gmail.com>
2025-08-20 15:51:04 +0800

    CountProperties取消@Validated注解

--- src/main/java/com/future/count/CountProperties.java
+++ src/main/java/com/future/count/CountProperties.java
@@ -2,11 +2,9 @@ package com.future.count;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
-@Validated
 @ConfigurationProperties(prefix = "spring.future.count")
 @Data
 public class CountProperties {
